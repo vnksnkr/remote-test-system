6d4a2537bb13436216bbeb17dbb6795708d2eb0c
Vinayak <vnksnkr@gmail.com>
2021-08-11 16:57:48 +0530

    add option for seed generation

--- remote-remote.py
+++ remote-remote.py
@@ -4,6 +4,7 @@ from smbus import SMBus
 from bitarray import bitarray
 from axiom_jtag import *
 from axiom_mxo2 import *
+import random
 
 class remote:
     P1 = "00000"
@@ -28,18 +29,25 @@ class remote:
     RESET_CMD = "10000011"
     SEED_CMD = "10000001"
     PARAM_CMD = "10000101"
+    RCV_CMD = "100"*16
+    
 
-
-    rand_seed = ""
+    
   
     def __init__(self,bus):
         self.i2c = SMBus(bus)
         self.jtag = JTag(self.i2c)
         self.def_addr = "00000"
+    
+    def generate_seed():
+        rand_seed= random.randrange(1,8192)
+        return bin(rand_seed)[2:].zfill(13)
 
     def on(self):
         self.jtag.on()
         self.jtag.reset()
+        self.jtag.sir(h2b("32"))
+        self.jtag.idle()
 
     def reset(self):
         self.jtag.reset()
@@ -50,33 +58,38 @@ class remote:
     def bounce_off(self):
         seed_s = '0'*13
         cmd = self.JTAG_CMD +'0'*20+ seed_s +self.SEED_CMD
-        self.jtag.cmdin("32",cmd)
+        self.jtag.cmdin(h2b("32"),cmd)
 
-    def loadseed(self,seed=rand_seed):
-        seed_s = h2b(seed).zfill(13)
-        cmd = self.JTAG_CMD +'0'*20+ seed_s +self.SEED_CMD
-        self.jtag.cmdin("32",cmd)
+    def loadseed(self,seed=None):
+        if seed == None:
+            seed_s = self.generate_seed()
+        else:
+            seed_s = h2b(seed).zfill(13)
+        cmd = self.JTAG_CMD +'0'*20+ seed_s + self.SEED_CMD
+        self.jtag.cmdin(h2b("32"),cmd)
 
-    def freq_div(duration):
-        div = ""
+    def freq_div(,seldduration):
+        div = bin(int(round((duration/1000)*2560000)))[2:].zfill(22)
+        print(div)
         return div
 
     def select(self,address):
         self.def_addr = address
 
-    def press(self,address=None,duration=0):
+    def press(self,address=None,duration=15):
         if address == None:
             address = self.def_addr
         if address == self.E1 or address == self.E2:
             print("Selected component is not button")
             return  0
-        press_count = h2b("1")[2:]
+
+        press_count = bin(1)[2:].zfill(6)
         cmd = self.JTAG_CMD + press_count + self.freq_div(duration) + address + self.PARAM_CMD
-        self.jtag.cmdin("32",cmd)
+        self.jtag.cmdin(h2b("32"),cmd)
         return 1
 
 
-    def turn(self,address=None,duration=0,ticks=0,counter=False):
+    def turn(self,address=None,duration=15,ticks=1,counter=False):
         if address == None:
             address = self.def_addr
 
@@ -84,10 +97,17 @@ class remote:
             print("Selected component is not encoder")
             return  0
 
-        ticks_s = h2b(ticks)[3:]
+        ticks_s = bin(ticks)[2:].zfill(6)
         if counter == True:
-            address[5] = address[:-1] + '1'
+            address = address[:-1] + '1'
         cmd = self.JTAG_CMD + ticks_s + self.freq_div(duration) + address + self.PARAM_CMD
+        self.jtag.cmdin(h2b("32"),cmd)
+        return 1
+
+    def wait(self):
+        cmdrcv = self.jtag.cmdout(h2b("32"),48)
+        while cmdrcv == self.RCV_CMD:
+            cmdrcv = self.jtag.cmdout(h2b("32"),48)
         return 1
     
 
