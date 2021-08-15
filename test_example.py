304d2010a65cd252549e7f6af4280fc073119d1c
Vinayak Sankar <vnksnkr@gmail.com>
2021-08-15 23:18:44 +0530

    Update test_example.py

--- test_example.py
+++ test_example.py
@@ -1,16 +1,16 @@
 from remote_remote import *
 
 testremote = remote(2)
-testremote.on() 						                                #initiates JTAG connection
-testremote.bounce_off()						                          #turns off bouncing 
-testremote.press(address=testremote.P13,duration=10) 		    #presses Push Button P13 for 10ms
-testremote.wait() 						                              #waits for acknowledge
-testremote.press(address=testremote.E1_S,duration=10) 		  #presses Encoder 1 Button for 10ms
+testremote.on()                                             #initiates JTAG connection
+testremote.bounce_off()                                     #turns off bouncing 
+testremote.press(address=testremote.P13,duration=10)        #presses Push Button P13 for 10ms
+testremote.wait()                                           #waits for acknowledge
+testremote.press(address=testremote.E1_S,duration=10)       #presses Encoder 1 Button for 10ms
 testremote.wait()
-testremote.turn(address=testremote.E2,ticks=2,duration=15) 	#turns Encoder 2, 2 ticks, each tick having pulse of duration 15ms
+testremote.turn(address=testremote.E2,ticks=2,duration=15)  #turns Encoder 2, 2 ticks, each tick having pulse of duration 15ms
 testremote.wait()
-testremote.loadseed()						                            #loads seed value(generated randomly) to switch on bouncing  
+testremote.loadseed()                                       #loads seed value(generated randomly) to switch on bouncing  
 testremote.press(address=testremote.P2,duration=10)
 testremote.wait()
-testremote.reset() 						                              #resets the internal logic of the gateware
-testremote.off() 						                                #closes JTAG connection
+testremote.reset()                                          #resets the internal logic of the gateware
+testremote.off()                                            #closes JTAG connection
